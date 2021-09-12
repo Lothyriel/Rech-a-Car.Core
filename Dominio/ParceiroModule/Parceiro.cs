723d5c298e47e1c31e2dc11348f6d6ceac9776bf
Lucas da Silva Costa <lucassilvacosta72@gmail.com>
2021-09-12 14:36:03 -0300

    Implementação das do Dominio Parceiro

---
+++ Dominio/ParceiroModule/Parceiro.cs
@@ -0,0 +1,33 @@
+﻿using Dominio.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.ParceiroModule
+{
+    public class Parceiro : Entidade
+    {
+        public Parceiro(string parceiro)
+        {
+            this.nome = parceiro;
+        }
+
+        public string nome { get; }
+        public override string Validar()
+        {
+            string resultadoValidacao = "";
+            if (string.IsNullOrEmpty(nome))
+                resultadoValidacao = "O Nome do Parceiro é obrigatório.";
+            if (resultadoValidacao == "")
+                resultadoValidacao = "ESTA_VALIDO";
+
+            return resultadoValidacao;
+        }
+        public override string ToString()
+        {
+            return nome;
+        }
+    }
+}
