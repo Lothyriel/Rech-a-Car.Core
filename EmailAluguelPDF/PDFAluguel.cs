0324fb65ec78b789dd903ccd644724ebaf0e6183
Victor Muniz <victor.hm2000@aluno.ifsc.edu.br>
2021-09-15 15:01:43 -0300

    melhorando o pdf

--- EmailAluguelPDF/PDFAluguel.cs
+++ EmailAluguelPDF/PDFAluguel.cs
@@ -4,6 +4,7 @@ using iText.IO.Font.Constants;
 using iText.IO.Image;
 using iText.Kernel.Font;
 using iText.Kernel.Pdf;
+using iText.Kernel.Pdf.Canvas.Draw;
 using iText.Layout;
 using iText.Layout.Element;
 using iText.Layout.Properties;
@@ -17,13 +18,16 @@ namespace EmailAluguelPDF
         public static void CriaEnvioEmail(Aluguel aluguel)
         {
             #region Estilos
-            Style helvetica20b = new();
+            Style helvetica32b = new Style();
             PdfFont fontHeader = PdfFontFactory.CreateFont(StandardFonts.HELVETICA_BOLD);
-            helvetica20b.SetFont(fontHeader).SetFontSize(20);
+            helvetica32b.SetFont(fontHeader).SetFontSize(32);
 
-            Style helvetica14r = new();
+            Style helvetica14r = new Style();
             PdfFont fontCorpo = PdfFontFactory.CreateFont(StandardFonts.HELVETICA);
             helvetica14r.SetFont(fontCorpo).SetFontSize(14);
+
+            SolidLine linha = new(1f);
+            LineSeparator separador = new(linha);
             #endregion
 
             var ms = new MemoryStream();
@@ -32,24 +36,35 @@ namespace EmailAluguelPDF
             var pdfDocument = new PdfDocument(writer);
             var pdf = new Document(pdfDocument);
 
-            Paragraph header = new Paragraph("RECH-A-CAR").SetTextAlignment(TextAlignment.CENTER).AddStyle(helvetica20b);
+            Paragraph header = new Paragraph("RECH-A-CAR").SetTextAlignment(TextAlignment.CENTER).AddStyle(helvetica32b);
             pdf.Add(header);
 
-            Paragraph corpo = new Paragraph().SetTextAlignment(TextAlignment.LEFT).AddStyle(helvetica14r);
-            corpo.Add(new Text($"Olá {aluguel.Cliente} aqui está o resumo do seu mais novo aluguel na Rech-a-car"));
-            corpo.Add(new Text($"Veículo: {aluguel.Veiculo}"));
-            corpo.Add(new Text($"Data de Aluguel: {aluguel.DataAluguel:d}"));
-            corpo.Add(new Text($"Data de Devolução: {aluguel.DataDevolucao:d}"));
-            corpo.Add(new Text($"Total Parcial R$: {aluguel.CalcularTotal()}"));
+            pdf.Add(separador);
 
+            Paragraph body_aluguel = new Paragraph().SetTextAlignment(TextAlignment.LEFT).AddStyle(helvetica14r);
+            body_aluguel.Add(new Text($"Olá, {aluguel.Cliente}. aqui está o resumo do seu mais novo aluguel na Rech-a-car!\n"));
+            body_aluguel.Add(new Text($"\tVeículo: {aluguel.Veiculo}\n"));
+            body_aluguel.Add(new Text($"\tData de Aluguel: {aluguel.DataAluguel:d}\n"));
+            body_aluguel.Add(new Text($"\tData de Devolução: {aluguel.DataDevolucao:d}\n"));
+            body_aluguel.Add(new Text($"\tTotal Parcial R$: {aluguel.CalcularTotal()}\n\n"));
+            pdf.Add(body_aluguel);
+
+            pdf.Add(separador);
+
+            Paragraph body_servicos = new Paragraph().SetTextAlignment(TextAlignment.LEFT).AddStyle(helvetica14r);
             if (aluguel.Servicos.Count > 0)
             {
-                corpo.Add(new Text($"Serviços alugados:"));
-                aluguel.Servicos.ForEach(s => corpo.Add(new Text($"{s}")));
+                body_servicos.Add(new Text($"Serviços alugados:"));
+                aluguel.Servicos.ForEach(s => body_servicos.Add(new Text($"\t{s}")));
             }
-            corpo.Add(ImagemItextImage(aluguel.Veiculo.Foto));
+            pdf.Add(body_servicos);
 
-            pdf.Add(corpo);
+            pdf.Add(separador);
+
+            Paragraph body_imagem = new Paragraph().SetTextAlignment(TextAlignment.CENTER).AddStyle(helvetica14r);
+            body_imagem.Add(ImagemItextImage(aluguel.Veiculo.Foto));
+            pdf.Add(body_imagem);
+
             pdf.Close();
             ControladorEmail.InserirParaEnvio(aluguel, ms);
         }
