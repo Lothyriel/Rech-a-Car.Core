acfeca9144c4246a6e45deaedd848bd4a390ee6d
lucas costa <lucassilvacosta72@gmail.com>
2021-09-29 17:13:09 -0300

    Testes Cnh

--- IntegrationTests/ClientePFModule/CnhDAO_Tests.cs
+++ IntegrationTests/ClientePFModule/CnhDAO_Tests.cs
@@ -20,5 +20,19 @@ namespace IntegrationTests.ClientePFModule
 
             CnhDAO.GetByIdCondutor(cnhAnterior.Id).TipoCnh.Should().Be(cnhnova.TipoCnh);
         }
+
+        [TestMethod]
+        public void Deve_Inserir_cnh_cliente()
+        {
+            var cnhAnterior = new CNH("1212120", TipoCNH.A);
+            CnhDAO.Inserir(cnhAnterior);
+        }
+
+        [TestMethod]
+        public void Deve_Excluir_cnh_cliente()
+        {
+            var cnhAnterior = new CNH("1212120", TipoCNH.A);
+            CnhDAO.Excluir()
+        }
     }
 }
