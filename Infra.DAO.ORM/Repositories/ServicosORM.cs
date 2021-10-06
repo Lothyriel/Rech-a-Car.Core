2edd20ef8710cfd605723d0888ae0b5b1600d691
Lucas da Silva Costa <lucassilvacosta72@gmail.com>
2021-10-06 17:28:38 -0300

    SO APANHEIR

---
+++ Infra.DAO.ORM/Repositories/ServicosORM.cs
@@ -0,0 +1,28 @@
+﻿using Dominio.ServicoModule;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra.DAO.ORM.Repositories
+{
+    public class ServicosORM : BaseRepository<Servico>, IServicoRepository
+    {
+        public void AlugarServicos(int id, List<Servico> servicos)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DesalugarServicosAlugados(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Servico> ServicosDisponiveis(int id)
+        {
+            //return Set<Servico>().Find(id != 0);
+            return Set<Servico>().Find(Id) != null;
+        }
+    }
+}
