36646e27762942cbda70f618775fdb1360b08ebe
Victor Muniz <victor.hm2000@aluno.ifsc.edu.br>
2021-10-07 15:59:11 -0300

    Adicionando configuration PJ

---
+++ Infra.DAO.ORM/Configurations/ClientePJConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Dominio.PessoaModule.ClienteModule;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infra.DAO.ORM.Configurations
+{
+    class ClientePJConfiguration : IEntityTypeConfiguration<ClientePJ>
+    {
+        public void Configure(EntityTypeBuilder<ClientePJ> builder)
+        {
+            builder.ToTable("TBClientePF");
+
+            builder.HasKey(p => p.Id);
+
+            builder.Property(p => p.Nome).HasColumnType("VARCHAR(80)").IsRequired();
+
+            builder.Property(p => p.Telefone).HasColumnType("CHAR(11)").IsRequired();
+
+            builder.Property(p => p.Documento).HasColumnType("CHAR(11)").IsRequired();
+
+            builder.Property(p => p.Email).HasColumnType("VARCHAR(80)").IsRequired();
+
+            builder.Property(p => p.Endereco).HasColumnType("VARCHAR(80)").IsRequired();
+
+            builder.HasMany(p => p.Motoristas);
+        }
+    }
+}
