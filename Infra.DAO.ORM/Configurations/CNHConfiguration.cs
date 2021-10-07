ee49784cbd9e663d7b5438861a4c042799fd4e1a
Victor Muniz <victor.hm2000@aluno.ifsc.edu.br>
2021-10-07 17:20:40 -0300

    adicionando configuration de cnh cupom e serviço

---
+++ Infra.DAO.ORM/Configurations/CNHConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Dominio.PessoaModule;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infra.DAO.ORM.Configurations
+{
+    class CNHConfiguration : IEntityTypeConfiguration<CNH>
+    {
+        public void Configure(EntityTypeBuilder<CNH> builder)
+        {
+            builder.ToTable("TBCnh");
+
+            builder.HasKey(p => p.Id);
+
+            builder.Property(p => p.NumeroCnh).HasColumnType("CHAR(11)").IsRequired();
+
+            builder.Property(p => p.TipoCnh).HasColumnType("VARCHAR(2)").IsRequired();
+        }
+    }
+}
