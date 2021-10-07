2b048ace829bddad9b4dea8ba7e74ed468e386b0
Lucas da Silva Costa <lucassilvacosta72@gmail.com>
2021-10-07 16:56:47 -0300

    Implementação SenhaORM

--- Infra.DAO.ORM/Repositories/SenhaORM.cs
+++ Infra.DAO.ORM/Repositories/SenhaORM.cs
@@ -7,17 +7,20 @@ namespace Infra.DAO.ORM.Repositories
     {
         public void Editar(int id_funcionario, string senha)
         {
-            throw new System.NotImplementedException();
+            Context.Update(senha);
+            Context.SaveChanges();
         }
 
         public Senha GetDadosSenha(int id_funcionario)
         {
-            throw new System.NotImplementedException();
+            return Context.Set<Senha>().Find(id_funcionario);
         }
 
         public void Inserir(int id_funcionario, string senha)
         {
-            throw new System.NotImplementedException();
+            var aa = Context.Set<Senha>(senha);
+            Context.Add(aa);
+            Context.SaveChanges();
         }
 
         public bool SenhaCorreta(int id_funcionario, string senha)
