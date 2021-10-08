487bf4f0b04ee22a47634a800c93b1680fe8254f
Victor Muniz <victor.hm2000@aluno.ifsc.edu.br>
2021-10-08 16:00:48 -0300

    orm -> sql

--- Apresentacao/Program.cs
+++ Apresentacao/Program.cs
@@ -25,7 +25,7 @@ namespace WindowsApp
             {
                 NLogger.Logger.Aqui().Info("Programa Iniciado");
 
-                new ConfigServices(ConfigRepositories.ORM, ConfigRelatorio.PDF);
+                new ConfigServices(ConfigRepositories.SQL, ConfigRelatorio.PDF);
 
                 new Login().Show();
 
