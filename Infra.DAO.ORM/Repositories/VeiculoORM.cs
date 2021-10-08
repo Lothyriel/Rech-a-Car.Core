d712a4f9bb2d3b6c119ddd557cf4bbb95e432848
Lucas da Silva Costa <lucassilvacosta72@gmail.com>
2021-10-08 17:06:22 -0300

    Implementação de VeiculoORM

--- Infra.DAO.ORM/Repositories/VeiculoORM.cs
+++ Infra.DAO.ORM/Repositories/VeiculoORM.cs
@@ -1,6 +1,10 @@
-﻿using Dominio.VeiculoModule;
+﻿using Dominio.AluguelModule;
+using Dominio.VeiculoModule;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Infra.DAO.ORM.Repositories
 {
@@ -8,12 +12,16 @@ namespace Infra.DAO.ORM.Repositories
     {
         public void AdicionarQuilometragem(Veiculo veiculo, int kmRodados)
         {
-            
+            veiculo.Quilometragem += kmRodados;
+            Context.Update(veiculo);
+            Context.SaveChanges();
         }
 
         public List<Veiculo> GetDisponiveis()
         {
-            throw new NotImplementedException();
+
+          return Context.Set<Aluguel>().Where(x => x.DataDevolucao < DateTime.Now).Select(a => a.Veiculo).ToList();
+
         }
     }
 }
