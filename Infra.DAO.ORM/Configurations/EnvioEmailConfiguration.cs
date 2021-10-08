f69b380f4619ee82b971308f3940d26fbe52802d
Victor Muniz <victor.hm2000@aluno.ifsc.edu.br>
2021-10-08 14:14:14 -0300

    adicionando configuration de senha e email

---
+++ Infra.DAO.ORM/Configurations/EnvioEmailConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Dominio.Shared;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infra.DAO.ORM.Configurations
+{
+    public class EnvioEmailConfiguration : IEntityTypeConfiguration<EnvioEmail>
+    {
+        public void Configure(EntityTypeBuilder<EnvioEmail> builder)
+        {
+            builder.ToTable("TBEmail");
+
+            builder.HasKey(p => p.Id);
+
+            builder.Property(p => p.Id_Aluguel).HasColumnType("INT").IsRequired();
+
+            builder.Property(p => p.DataEnvio).HasColumnType("DATE").IsRequired();
+
+            builder.Property(p => p.StreamAttachment).HasColumnType("VARBINARY(MAX)").IsRequired();
+        }
+    }
+}
---
+++ Infra.DAO.ORM/Configurations/SenhaConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Dominio.Entities.PessoaModule;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infra.DAO.ORM.Configurations
+{
+    class SenhaConfiguration : IEntityTypeConfiguration<Senha>
+    {
+        public void Configure(EntityTypeBuilder<Senha> builder)
+        {
+            builder.ToTable("TBSenha");
+
+            builder.HasKey(p => p.Id);
+
+            builder.Property(p => p.Salt).HasColumnType("BINARY(16)").IsRequired();
+
+            builder.Property(p => p.Hash).HasColumnType("STRING").IsRequired();
+        }
+    }
+}
