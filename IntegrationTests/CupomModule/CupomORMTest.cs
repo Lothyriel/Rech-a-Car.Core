89442261afce362fecc050f04a05c331f75db8aa
Lucas da Silva Costa <lucassilvacosta72@gmail.com>
2021-10-13 16:41:57 -0300

    Implementado ParceiroORMTest

---
+++ IntegrationTests/CupomModule/CupomORMTest.cs
@@ -0,0 +1,30 @@
+﻿using Dominio.CupomModule;
+using Dominio.ParceiroModule;
+using Infra.DAO.ORM.Repositories;
+using Infra.DAO.Shared;
+using IntegrationTests.Shared;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegrationTests.CupomModule
+{
+    [TestClass]
+    public class CupomORMTest
+    {
+        CupomORM cupomORM = new();
+        [TestCleanup]
+        public void Limpar()
+        {
+            Db.Delete(TestExtensions.ResetId("TBCupons"));
+        }
+        [TestMethod]
+        public void Deve_Inserir_Novo_Cupom()
+        {
+
+        }
+    }
+}
