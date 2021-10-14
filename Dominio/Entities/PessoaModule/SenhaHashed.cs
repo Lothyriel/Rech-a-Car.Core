f8b65fe22c421f0d7213cc53ee022d86975999ca
João Xavier <fastjonh@gmail.com>
2021-10-14 04:13:39 -0300

    refatoracao na classe da senha { movi os metodos estaticos para o dominio inves do repositorio pois fazia mais sentido
    criei metodos para verificar a senha correta direto do dominio para poder reutilizar em diversos repositorios
    }

---
+++ Dominio/Entities/PessoaModule/SenhaHashed.cs
@@ -0,0 +1,53 @@
+﻿using Dominio.Shared;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System;
+using System.Security.Cryptography;
+
+namespace Dominio.Entities.PessoaModule
+{
+    public class SenhaHashed : Entidade
+    {
+        public SenhaHashed(byte[] salt, string hash)
+        {
+            Salt = salt;
+            Hash = hash;
+        }
+        public byte[] Salt { get; }
+        public string Hash { get; }
+
+        public override string Validar()
+        {
+            return "";
+        }
+
+        private static byte[] GerarSalt()
+        {
+            byte[] salt = new byte[128 / 8];
+            using (var rngCsp = new RNGCryptoServiceProvider())
+            {
+                rngCsp.GetNonZeroBytes(salt);
+            }
+
+            return salt;
+        }
+        private static string GerarHash(string senha, byte[] salt)
+        {
+            return Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                                        password: senha,
+                                        salt: salt,
+                                        prf: KeyDerivationPrf.HMACSHA256,
+                                        iterationCount: 100000,
+                                        numBytesRequested: 256 / 8));
+        }
+        public static bool SenhaCorreta(string senha, SenhaHashed hashed) 
+        {
+            return GerarHash(senha, hashed.Salt) == hashed.Hash;
+        }
+        public static SenhaHashed GerarNovaSenhaHashed(string senha) 
+        {
+            var salt = GerarSalt();
+            var hashed = GerarHash(senha, salt);
+            return new SenhaHashed(salt, hashed);
+        }
+    }
+}
--- Dominio/Repositories/ISenhaRepository.cs
+++ Dominio/Repositories/ISenhaRepository.cs
@@ -7,29 +7,9 @@ namespace Dominio.Repositories
 {
     public interface ISenhaRepository
     {
-        Senha GetDadosSenha(int id_funcionario);
+        SenhaHashed GetSenhaHashed(int id_funcionario);
         bool SenhaCorreta(int id_funcionario, string senha);
         void Inserir(int id_funcionario, string senha);
         void Editar(int id_funcionario, string senha);
-
-        public static byte[] GerarSalt()
-        {
-            byte[] salt = new byte[128 / 8];
-            using (var rngCsp = new RNGCryptoServiceProvider())
-            {
-                rngCsp.GetNonZeroBytes(salt);
-            }
-
-            return salt;
-        }
-        public static string GerarHash(string senha, byte[] salt)
-        {
-            return Convert.ToBase64String(KeyDerivation.Pbkdf2(
-                                        password: senha,
-                                        salt: salt,
-                                        prf: KeyDerivationPrf.HMACSHA256,
-                                        iterationCount: 100000,
-                                        numBytesRequested: 256 / 8));
-        }
     }
 }
